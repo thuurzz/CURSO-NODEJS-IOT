d4d32c0649e2b6c809bcd58bec9a00c2d877aae9
Arthur Vinicius <thuur.vss@gmail.com>
2021-11-18 14:42:50 -0300

    async/await

--- async/async.js
+++ async/async.js
@@ -1,14 +1,23 @@
-// promise
-function soma(x){
+// Função com promise
+function sum(x){
     return new Promise((resolve, reject)=>{    
+        if (Number(x == NaN || Number(x == undefined || typeof x != "number"))){
+            reject("Tá de brincadeira?")
+        }
         setTimeout(()=>{
             resolve(x + 5000);
         }, 3000);
     })
 }
+// Função main chamando a função com await
+async function main(){
+   try {
+        const res = await sum("#")
+        console.log(`Resultado com Async/Await: ${res}`)
+   }
+    catch (error){
+        console.log(`Temos um problema: ${error}`)
+    }
+}
+main()
 
-// then pega o retorno da promisse quando da certo
-soma(230)
-    .then((resultado)=>{
-        console.log(`Resolvido, resultado: ${resultado}`)
-    })
